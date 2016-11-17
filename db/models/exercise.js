f1407da32c489292841e736e77cf744a37102eae
Emily H <emilyyho33@gmail.com>
2016-11-17 18:01:51 -0500

    exercise model made, default exercise photo added

--- db/models/exercise.js
+++ db/models/exercise.js
@@ -1,9 +1,37 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require('sequelize');
+const db = require('../db');
 
-const ModelName = db.define('exercise', {
+const Exercise = db.define('exercise', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate : {
+      notEmpty: true
+    }
+  },
+  description : {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate:{
+      notEmpty:true
+    }
+  },
+  imgURL : {
+    type: Sequelize.STRING,
+    defaultValue:"", // add photo from static folder
+    validate:{
+      isURL: true
+    }
+  },
+  vidURL : {
+    type: Sequelize.STRING,
+    defaultValue:"", // add photo from static folder
+    validate:{
+      isURL: true
+    }
+  }
 
-})
+});
 
 
-module.exports = ModelName;
+module.exports = Exercise;
