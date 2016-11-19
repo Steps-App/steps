3ecb1bddce947cb31e276df65acf8ea463e486e5
Hitenp1 <hpduke2@gmail.com>
2016-11-19 17:20:02 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,11 +12,6 @@ router
   .use('/patient', patient)
   .use('/routetemplate', routetemplate)
 
-// Send along any errors
-router.use((err, req, res, next) => {
-  res.status(err.status || 500).send(err.message || err)
-})
-
 // No API routes matched? 404.
 router.use((req, res) => res.status(404).end())
 
