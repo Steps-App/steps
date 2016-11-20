2be5065321630675ac6e21e485453a515a03faa0
mkewls <mw@mikewill.net>
2016-11-20 14:41:39 -0500

    modifications to plan/patient models, patient test, patient route

--- tests/patient.test.js
+++ tests/patient.test.js
@@ -6,11 +6,11 @@ import bcrypt from 'bcrypt'
 import db from '../db'
 import Patient from '../db/models/patient'
 
-import app from '../server/app';
-import Promise from 'bluebird';
-import supertest from 'supertest';
+import app from '../server/app'
+import Promise from 'bluebird'
+import supertest from 'supertest'
 
-const agent = supertest.agent(app);
+const agent = supertest.agent(app)
 
 describe('Patient', function () {
 
@@ -31,7 +31,6 @@ describe('Patient', function () {
     email: 'joe@gmail.com',
     DOB: '10/14/1976',
     gender: 'M',
-    password: 'badpassword'
   }
 
   let invalidPatient = {
@@ -40,17 +39,16 @@ describe('Patient', function () {
     email: 'joe@gmail.com',
     DOB: '10/14/1976',
     gender: 'M',
-    password: 'badpassword'
   }
 
   describe('Patient Model: ', function() {
 
     describe('creates patients and properly validates: ', function() {
 
-      it('creates a therapist row with valid input', function() {
+      it('creates a patient with valid input', function() {
         return Patient.create(validPatient)
-          .then(createdRow => {
-            expect(createdRow).to.contain({ first_name: 'Joe', last_name: 'Patient', email: 'joe@gmail.com' })
+          .then(createdPatient => {
+            expect(createdPatient).to.contain({ first_name: 'Joe', last_name: 'Patient', email: 'joe@gmail.com' })
           })
           .catch(err => console.log(err))
       })
@@ -69,7 +67,7 @@ describe('Patient', function () {
 
     describe('hooks', function(){
 
-      it('should set email to lowercase and return a password_digest', function() {
+      it('should set email to lowercase and return a password_digest from a randomly-generated password', function() {
         return Patient.findOne({
             where: {
               email: validPatient.email
@@ -92,7 +90,12 @@ describe('Patient', function () {
             }
           })
           .then(foundPatient => {
-            let passAuth = bcrypt.compareSync('badpassword', foundPatient.dataValues.password_digest, (err, result) => {
+            return Promise.all([foundPatient, foundPatient.update({
+              password: 'badpassword'
+            })])
+          })
+          .spread((foundPatient, updatedPatient) => {
+            let passAuth = bcrypt.compareSync('badpassword', updatedPatient.dataValues.password_digest, (err, result) => {
                 return result
             })
             expect(passAuth).to.equal(true)
@@ -103,43 +106,39 @@ describe('Patient', function () {
   })
 
   describe('Patient Route', () => {
-    
+
     describe('Patients', () => {
 
       it('GET all patients', function () {
         return agent
         .get('/api/patient')
         .expect(200)
-      });
+      })
 
       it('POST one Patient', function (done) {
         agent
         .post('/api/patient')
+        .set('Content-type', 'application/json')
         .send({
-          'first_name': 'PatientTestF1',
-          'last_name': 'PatientTestL1',
-          'email': 'patient1@test.com',
-          'DOB': '11/18/15',
-          'gender': 'F'
+          first_name: 'PatientTestF1',
+          last_name: 'PatientTestL1',
+          email: 'patient1@test.com',
+          DOB: '11/18/15',
+          gender: 'F',
         })
         .expect(201)
         .end(function (err, res) {
-          console.log('sdfsdfsdfsd')
-          console.log('Error in Posting', err)
-
-          if (err) return done(err);
-          expect(res.body.first_name).to.equal('PatientTestF1');
-          expect(res.body.id).to.exist;
-          Patient.findById(res.body.id)
+          if (err) return done(err)
+          expect(res.body.first_name).to.equal('PatientTestF1')
+          expect(res.body.id).to.exist
+          return Patient.findById(res.body.id)
             .then(function (b) {
-              expect(b).to.not.be.null;
-              expect(res.body).to.eql(toPlainObject(b));
+              expect(b).to.not.be.null
               done()
             })
-            .catch(done);
+            .catch(done)
         })
-        .catch(err => console.log(err))
-      });
+      })
     })
   })
 })
