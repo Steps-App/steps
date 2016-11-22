baeadeb7ed437d58e908c07d0ad445fb1c04a155
Hitenp1 <hpduke2@gmail.com>
2016-11-21 21:52:56 -0500

    frontendredux (#91)

--- app/main.js
+++ app/main.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 
 // Redux Actions and Thunks
 import store from './store'
+import { retrieveLoggedInUser } from './reducers/user'
 
 // React Compontents 
 import Home from './components/home/Home'
@@ -15,7 +16,7 @@ import PatientDash from './components/patients/PatientDash'
 
 // React router hooks
 const appEnter = (nextState, replace, callback) => {
-  // hit /api/auth/me to get logged in user
+  store.dispatch(retrieveLoggedInUser())
   // If user is not logged in, redirect them to the home page
   if (!Object.keys(store.getState().user).length)
     replace('/');
