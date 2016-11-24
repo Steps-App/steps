b2fec92ce750bbba1a53b68dfe8f00e32e5c3f6a
Jake Peyser <jakepeyser@gmail.com>
2016-11-24 12:27:45 -0500

    Refactored seed and all tables except Workouts

--- db/Seed/seedbuilder.js
+++ db/Seed/seedbuilder.js
@@ -3,14 +3,16 @@ if (process.env.NODE_ENV !== 'production')
   require('dotenv').config();
 
 const Promise = require('bluebird');
+const chalk = require('chalk');
+
 // db + models
 const db = require('../../db/');
-const therapistModel  = db.model('therapist');
-const patientModel  = db.model('patient');
-const exerciseModel = db.model('exercise');
-const planModel = db.model('plan')
-const treatmentModel = db.model('treatment')
-const workoutModel = db.model('workout')
+const Therapist  = db.model('therapist');
+const Patient  = db.model('patient');
+const Exercise = db.model('exercise');
+const Plan = db.model('plan')
+const Treatment = db.model('treatment')
+const Workout = db.model('workout')
 
 // seeds
 const TherapistList = require('./therapistSeed');
@@ -20,24 +22,74 @@ const PlanList = require('./planSeed')
 const TreatmentList = require('./treatmentSeed')
 const WorkoutList = require('./workoutSeed')
 
+// Returns array of random instances and removes them from original array
+const randomInstances = (count, list) => {
+  const instances = [];
+  for(let i = 0; i < count; i++) {
+    if (!list.length) break;
+    const useInd = Math.floor(Math.random() * list.length);
+    instances.push(list[useInd]);
+    list.splice(useInd, 1);
+  }
+  return instances;
+}
+
+let createdTherapists;
 db.didSync
-  .then(() => db.sync({force:true}))
-  .then(() => Promise.map(TherapistList, (therapist) => db.model('therapist').create(therapist)))
-  .then(() => Promise.map(PatientList, (patient) => db.model('patient')
-    .create(Object.assign(patient, {therapist_id: Math.floor((Math.random() * TherapistList.length) + 1)}))))
-  .then(() => Promise.map(ExerciseList,(exercise) => db.model("exercise")
-     .create(Object.assign(exercise,{therapist_id: Math.floor((Math.random() * TherapistList.length) + 1)}))))
-  .then(() => planModel.bulkCreate(PlanList))
-  .then(() => treatmentModel.bulkCreate(TreatmentList))
-  .then(() => workoutModel.bulkCreate(WorkoutList))
-  .then(function () {
-    console.log("Finished Seeding Database");
+  .then(() => db.sync({ force: true }))
+  // Create therapists and patients
+  .then(() => {
+    return Promise.all([
+      Promise.map(TherapistList, therapist => Therapist.create(therapist)),
+      Promise.map(PatientList, patient => Patient.create(patient)),
+      Promise.map(ExerciseList, exercise => Exercise.create(exercise)),
+      Promise.map(PlanList, plan => Plan.create(plan)),
+      Promise.map(TreatmentList, treatment => Treatment.create(treatment))
+    ])
   })
-  .catch(function (err) {
-    console.error('Issue with Seeding', err, err.stack);
+  // Assign patients and random exercises to the therapists
+  // Assign plans to patients
+  .spread((therapists, patients, exercises, plans) => {
+    const updatedTherapists = [];
+    patients.forEach((patient, i) => {
+      patient.setPlans(randomInstances(2, plans));
+    })
+    therapists.forEach((therapist, i) => {
+      therapist.setPatients(randomInstances(20, patients));
+      updatedTherapists.push(therapist.setExercises(randomInstances(20, exercises)));
+    })
+    return Promise.all(updatedTherapists)
   })
+  .then(() => {
+    return Promise.all([
+      Treatment.findAll(),
+      Plan.findAll({
+        include: [{
+          model: Patient,
+          include:[{ model: Therapist, include:[Exercise] }]
+        }]
+      })
+    ])
+  })
+  .spread((treatments, plans) => {
+    const updatedPlans = [];
+    for (let i=0; i < plans.length; i++) {
+      const planExercises = plans[i].patient.therapist.exercises;
+      const randomTreatments = randomInstances(5, treatments);
+      for (let j=0; j < randomTreatments.length; j++) {
+        randomTreatments[j].setExercise(...randomInstances(1, planExercises));
+        randomTreatments[j].update({patient_id: plans[i].patient_id });
+
+      }
+      updatedPlans.push(plans[i].setTreatments(randomTreatments));
+    }
+    return Promise.all(updatedPlans);
+  })
+  // .then(() => Workout.bulkCreate(WorkoutList))
+  .then(() => console.log(chalk.green("Finished Seeding Database")))
+  .catch(err => console.error(chalk.red('Issue with Seeding', err, err.stack)))
   .finally(function () {
     db.close();
-    console.log('Connection Closed');
+    console.log(chalk.yellow('Connection closed'));
     return null; // silences bluebird warning about using non-returned promises inside of handlers.
   });
--- db/models/index.js
+++ db/models/index.js
@@ -9,10 +9,13 @@ const Workout = require('./workout')
 // -=-=-=-=-=-= ASSOCIATIONS =-=-=-=-=-=-
 // http://docs.sequelizejs.com/en/latest/docs/associations/
 Therapist.hasMany(Patient) // patient << therapist_id // therapist.getPatients/setPatients
+Patient.belongsTo(Therapist);
 Exercise.belongsTo(Therapist, { foreignKey: 'therapist_id' }) // exercise << therapist_id
+Therapist.hasMany(Exercise); // exercise << therapist_id
 
 Patient.hasMany(Workout) // workout << patient_id // patient.getWorkouts/setWorkouts
 Plan.hasMany(Workout) // workout << plan_id // plan.getWorkouts/setWorkouts
+Plan.belongsTo(Patient) // workout << plan_id // plan.getWorkouts/setWorkouts
 Plan.hasMany(Treatment) // treatment << plan_id // plan.getTreatments/setTreatments
 
 Patient.hasMany(Treatment) // treatment << patient_id // patient.getTreatments/setTreatments
