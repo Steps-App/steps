46dd25d70913164621ca49e22c79cce36e9bdefa
Emily Ho <EmilyHo@Jimmys-MacBook-Pro.local>
2016-11-27 18:28:47 -0500

    about us added

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -8,11 +8,15 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 //External Components Modular
 import Signin from './Signin'
 
+//About us
+const defaultPix = require("../../../src/images/defaultProfile.png");
+
 // -=-=-=-=-=-= COMPONENT =-=-=-=-=-=-
 
 export default () => (
   <MuiThemeProvider>
-    <div id="home">
+    <div className="container" id="h">
+    <div className="container" id="home">
       <nav className="home-nav">
         <div className="navbar-header">
           <a href="#">Steps</a>
@@ -22,11 +26,11 @@ export default () => (
           <li><a href="#">Contact</a></li>
         </ul>
       </nav>
-      <div className="main">
+      <div className="container" id="main">
         <div className="row">
           <div className="col-xs-5 intro-text">
             <p className="intro-heading" >Physical therapy for the way you live today</p>
-            <p className="intro-content">Plans starting as low as $500 per practice</p>
+            <p className="intro-content">We help you make the steps to recovery</p>
           </div>
           <div className="col-xs-offset-2 col-xs-4">
             <Signin />
@@ -34,5 +38,31 @@ export default () => (
         </div>
       </div>
     </div>
+
+    <div className="container" id="aboutus">
+      <div className="aboutus" id="whoami">
+        <h3> About Us</h3>
+      </div>
+      <div className="row" id="us">
+        <div className="col-md-3" id="jake">
+          <img src={defaultPix}/>
+          <p>Hi, I am Jake Peyser</p>
+        </div>
+        <div className="col-md-3" id="emily">
+          <img src={defaultPix}/>
+          <p>Hi, I am Mike</p>
+        </div>
+        <div className="col-md-3" id="mike">
+          <img src={defaultPix}/>
+          <p>Hi, Emily</p>
+        </div>
+        <div className="col-md-3" id="hiten">
+          <img src={defaultPix}/>
+          <p>Hi, Hiten</p>
+        </div>
+      </div>
+    </div>
+</div>
+
   </MuiThemeProvider>
 )
