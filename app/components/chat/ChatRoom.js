ce0ab1008ac8602bec457915ca6fe6acbb35a68b
mkewls <mw@mikewill.net>
2016-11-28 12:04:32 -0500

    same commit

--- app/components/chat/ChatRoom.js
+++ app/components/chat/ChatRoom.js
@@ -14,7 +14,7 @@ import { Paper } from 'material-ui'
 // -=-=-=-=-=-= COMPONENT =-=-=-=-=-=-
 
 // socket global
-let socket = io.connect(`http://localhost:8080/1`)
+let socket = io.connect(`http://localhost:8080/3`)
 
 export class ChatRoom extends Component {
 
