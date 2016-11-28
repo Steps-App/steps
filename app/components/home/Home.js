e4e4cc09868f789153ec7dd8d0545640be11cbbe
Emily Ho <EmilyHo@Jimmys-MacBook-Pro.local>
2016-11-28 00:16:34 -0500

    about us  basbasic layout made

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 
 // Material theme
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Paper} from 'material-ui';
 
 //External Components Modular
 import Signin from './Signin'
 
 //About us
 const defaultPix = require("../../../src/images/defaultProfile.png");
+
 
 // -=-=-=-=-=-= COMPONENT =-=-=-=-=-=-
 
@@ -22,7 +24,7 @@ export default () => (
           <a href="#">Steps</a>
         </div>
         <ul className="navbar-links">
-          <li><a href="#">About Us</a></li>
+          <li><a href="#aboutus">About Us</a></li>
           <li><a href="#">Contact</a></li>
         </ul>
       </nav>
@@ -41,28 +43,44 @@ export default () => (
 
     <div className="container" id="aboutus">
       <div className="aboutus" id="whoami">
-        <h3> About Us</h3>
+        <h2> About Us</h2>
       </div>
+      <div className="row" id="aboutinfo">
+         <div className="col-xs-12 col-md-6">
+           <img src={require="../../../src/images/work.jpg"} id="work"></img>
+         </div>
+         <div className="col-xs-12 col-md-6">
+           <div className="aboutcontent" >
+               <h4 className="row">Who are we?</h4>
+               <p className="row"> We are a group of Students who are at Fullstack Academy blah balh blahdsflkajdlkfjdijsadlnn kldjslafjl dsfsldkfj s</p>
+           </div>
+         </div>
+      </div>
+      <h2> Meet Us </h2>
       <div className="row" id="us">
-        <div className="col-md-3" id="jake">
+        <div className="col-xs-12 col-sm-6 col-md-3" id="jake">
+          <h4>Jake</h4>
           <img src={defaultPix}/>
           <p>Hi, I am Jake Peyser</p>
         </div>
-        <div className="col-md-3" id="emily">
+        <div className="col-xs-12 col-sm-6 col-md-3" id="emily">
+          <h4>Emily</h4>
           <img src={defaultPix}/>
           <p>Hi, I am Mike</p>
         </div>
-        <div className="col-md-3" id="mike">
+        <div className="col-xs-12 col-sm-6 col-md-3" id="mike">
+          <h4>Mike</h4>
           <img src={defaultPix}/>
           <p>Hi, Emily</p>
         </div>
-        <div className="col-md-3" id="hiten">
+        <div className="col-xs-12 col-sm-6 col-md-3" id="hiten">
+          <h4>Hiten</h4>
           <img src={defaultPix}/>
           <p>Hi, Hiten</p>
         </div>
       </div>
     </div>
-</div>
+  </div>
 
   </MuiThemeProvider>
 )
