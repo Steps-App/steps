3ec576290f0b62e4870d6d38526dffc78465271a
Hitenp1 <hpduke2@gmail.com>
2016-11-28 22:15:23 -0500

    therapistexercises

---
+++ app/components/exercises/ExerciseListContainer.js
@@ -0,0 +1,87 @@
+// import redux, react
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
+import { Link, browserHistory } from 'react-router';
+import { deleteExercise } from '../../reducers/exercises'
+
+//Material UI
+import { Table, TableHeader, TableHeaderColumn,
+         TableBody, TableRow, TableRowColumn, TableFooter} from 'material-ui'
+
+import { StepsRaisedButton } from '../material-style'
+
+// -=-=-=-=-=-= COMPONENT =-=-=-=-=-=-
+
+export class ExerciseList extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+
+    const { exercises, deleteExercise } = this.props;
+
+    return (
+     
+      <div id="exercise-list" className="col-xs-12">
+          <Helmet title="Exercise List" />
+          <h1 className="page-header">Exercise List</h1>
+  
+          <StepsRaisedButton
+            label="Add New Exercise"
+            backgroundColor="#005B96"
+            labelStyle={{color: 'white'}}
+            style={{marginBottom: '20px', float: 'right'}}
+              />
+          <div className="table">
+          <Table style={{backgroundColor:'none'}} >
+            <TableBody displayRowCheckbox={false}>
+            {
+              exercises && exercises.map( exercise =>
+                <TableRow key={ exercise.id }>
+                    <TableRowColumn style={{ width: "30%" }}>
+                      <img className="img-responsive" src={exercise.img_url}></img>
+                    </TableRowColumn>
+                    <TableRowColumn style={{wordWrap: 'break-word', whiteSpace: 'normal'}} >
+                        <p style={{fontWeight: "bold", fontSize: "larger" }}>{`${exercise.title}`}</p>
+                        <p><span style={{fontWeight: "bold"}}>Description</span>{`: ${exercise.description}`}</p>
+                      </TableRowColumn>
+                    <TableRowColumn >
+                        <StepsRaisedButton
+                          label="Delete"
+                          backgroundColor="#ff0000"
+                          labelStyle={{color: 'white'}}
+                          onClick={() => deleteExercise(exercise.id)}
+                        />
+                    </TableRowColumn>
+                </TableRow>
+              )
+            }
+           </TableBody>
+          </Table>
+          </div>
+        </div>
+    )
+  }
+}
+
+// -=-=-=-=-= CONTAINER =-=-=-=-=-=-
+
+const mapStateToProps = ({ exercises }) => ({ exercises })
+
+const mapDispatchtoProps = dispatch => ({
+  createExercise : (newExercise) => dispatch(createExercise(newExercise)),
+  deleteExercise : (exerciseId) => dispatch(deleteExercise(exerciseId))
+})
+
+export default connect(mapStateToProps, mapDispatchtoProps)(ExerciseList);
+
+
+
+
+
+
+
