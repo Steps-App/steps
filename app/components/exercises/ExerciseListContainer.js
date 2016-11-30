fd1bab958a0f3f1ca2ad4dd616f5b1c5ddea3d37
Emily H <emilyyho33@gmail.com>
2016-11-30 13:22:53 -0500

    remove icons hidden

--- app/components/exercises/ExerciseListContainer.js
+++ app/components/exercises/ExerciseListContainer.js
@@ -7,7 +7,7 @@ import { deleteExercise } from '../../reducers/exercises'
 
 //Material UI
 import { Table, TableHeader, TableHeaderColumn,
-         TableBody, TableRow, TableRowColumn, TableFooter} from 'material-ui'
+         TableBody, TableRow, TableRowColumn, TableFooter, IconButton, FontIcon} from 'material-ui'
 
 import { StepsRaisedButton } from '../material-style'
 
@@ -17,7 +17,15 @@ export class ExerciseList extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      showRemove :false
+    }
+    this.showRemove = this.showRemove.bind(this);
+
+  }
+
+  showRemove (){
+    this.setState({showRemove:!this.state.showRemove});
   }
 
   render() {
@@ -25,37 +33,45 @@ export class ExerciseList extends Component {
     const {user, exercises, deleteExercise } = this.props;
 
     return (
-     
-      <div id="exercise-list" className="col-xs-12">
+
+      <div id="exercise-list" >
           <Helmet title="Exercise List" />
           <h1 className="page-header">Exercise List</h1>
-  
+          <div>
+          <StepsRaisedButton
+            label="Delete"
+            backgroundColor="#D9534F"
+            labelStyle={{color: 'white'}}
+            onClick={this.showRemove}
+          />
           <StepsRaisedButton
             label="Add New Exercise"
             backgroundColor="#005B96"
             labelStyle={{color: 'white'}}
             style={{marginBottom: '20px', float: 'right'}}
               />
+            </div>
           <div className="table">
           <Table style={{backgroundColor:'none'}} >
             <TableBody displayRowCheckbox={false}>
             {
               exercises && exercises.map( exercise =>
                 <TableRow key={ exercise.id } selectable={false}>
-                    <TableRowColumn style={{ width: "30%" }}>
-                      <img className="img-responsive" src={exercise.img_url}></img>
+                    <TableRowColumn>
+                      <img src={exercise.img_url}></img>
                     </TableRowColumn>
                     <TableRowColumn style={{wordWrap: 'break-word', whiteSpace: 'normal'}} >
-                        <p style={{fontWeight: "bold", fontSize: "larger" }}>{`${exercise.title}`}</p>
+                      <div className="exercise-title">
+                        <h3 className="col-md-6" >{exercise.title}</h3>
+
+                        <IconButton id={this.state.showRemove ? "show" : "remove"} tooltip="Permanent" iconClassName="material-icons" onClick={() => deleteExercise(user.id, exercise.id)}>
+                          highlight_off
+                        </IconButton>
+                      </div>
+                      <div className="exercise-description">
                         <p><span style={{fontWeight: "bold"}}>Description</span>{`: ${exercise.description}`}</p>
-                      </TableRowColumn>
-                    <TableRowColumn >
-                        <StepsRaisedButton
-                          label="Delete"
-                          backgroundColor="#ff0000"
-                          labelStyle={{color: 'white'}}
-                          onClick={() => deleteExercise(user.id, exercise.id)}
-                        />
+
+                      </div>
                     </TableRowColumn>
                 </TableRow>
               )
@@ -78,10 +94,3 @@ const mapDispatchtoProps = dispatch => ({
 })
 
 export default connect(mapStateToProps, mapDispatchtoProps)(ExerciseList);
-
-
-
-
-
-
-
