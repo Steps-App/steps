2026628f2896dcd4ad5738a152a80f86f076bced
Jake Peyser <jakepeyser@gmail.com>
2016-12-02 18:19:27 -0500

    main home page section responsive

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -22,86 +22,88 @@ const navbarLinks = {
 
 export default () => (
   <MuiThemeProvider>
-    <div className="container" id="h">
-    <div className="container" id="home">
-      <nav className="home-nav">
-        <div className="navbar-header">
-          <a href="#">
-            <svg width="45px" height="30px" viewBox="0 0 204 137">
-              <g id="Page-1" stroke="none" strokeWidth="1" fill="none">
-                <g id="Group" transform="translate(0.000000, -0.362180)" fill="#FFFFFF">
-                  <path d="M0,108.36218 L204,108.36218 L204,137.36218 L0,137.36218 L0,108.36218 Z M2,110.36218 L202,110.36218 L202,135.36218 L2,135.36218 L2,110.36218 Z" id="Shape"></path>
-                  <path d="M50,54.36218 L204,54.36218 L204,83.36218 L50,83.36218 L50,54.36218 Z M52,56.36218 L202,56.36218 L202,81.36218 L52,81.36218 L52,56.36218 Z" id="Shape"></path>
-                  <path d="M100,0.36218 L204,0.36218 L204,29.36218 L100,29.36218 L100,0.36218 Z M102,2.36218 L202,2.36218 L202,27.36218 L102,27.36218 L102,2.36218 Z" id="Shape"></path>
+    <div id="main">
+      <div className="container" id="home">
+        <nav className="home-nav">
+          <div className="navbar-header">
+            <a href="#">
+              <svg width="45px" height="30px" viewBox="0 0 204 137">
+                <g id="Page-1" stroke="none" strokeWidth="1" fill="none">
+                  <g id="Group" transform="translate(0.000000, -0.362180)" fill="#FFFFFF">
+                    <path d="M0,108.36218 L204,108.36218 L204,137.36218 L0,137.36218 L0,108.36218 Z M2,110.36218 L202,110.36218 L202,135.36218 L2,135.36218 L2,110.36218 Z" id="Shape"></path>
+                    <path d="M50,54.36218 L204,54.36218 L204,83.36218 L50,83.36218 L50,54.36218 Z M52,56.36218 L202,56.36218 L202,81.36218 L52,81.36218 L52,56.36218 Z" id="Shape"></path>
+                    <path d="M100,0.36218 L204,0.36218 L204,29.36218 L100,29.36218 L100,0.36218 Z M102,2.36218 L202,2.36218 L202,27.36218 L102,27.36218 L102,2.36218 Z" id="Shape"></path>
+                  </g>
                 </g>
-              </g>
-            </svg>
-            <h1>Steps</h1>
-          </a>
-        </div>
-        <ul className="navbar-links">
-        {
-          Object.keys(navbarLinks).map((key, i) => <li key={i}><a href={`#${key}`}>{key.link}</a></li>)
-        }
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-      <div className="container" id="intro">
-        <div className="row">
-          <div className="col-xs-5 intro-text">
-            <p className="intro-heading" >Physical therapy for the way you live today</p>
-            <p className="intro-content">We help you make the steps to recovery</p>
+              </svg>
+              <h1>Steps</h1>
+            </a>
           </div>
-          <div className="col-xs-offset-2 col-xs-4">
+          <ul className="navbar-links">
+          {
+            Object.keys(navbarLinks).map((key, i) =>
+              <li key={i}>
+                <a href={`#${key}`}>{navbarLinks[key]}</a>
+              </li>
+            )
+          }
+          </ul>
+        </nav>
+        <div className="intro">
+          <div className="intro-content">
+            <div className="intro-text">
+              <p className="intro-heading" >Physical therapy for the way you live today</p>
+              <p className="intro-content">We help you make the steps to recovery</p>
+            </div>
+            <div className="signup-form">
             <Signin />
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div className="container" id="about">
-      <div className="aboutus" id="whoami">
-        <h2> About Us</h2>
-      </div>
-      <div className="row" id="aboutinfo">
-         <div className="col-xs-12 col-md-6">
-           <img src={require('../../../src/images/work.jpg')} id="work"></img>
-         </div>
-         <div className="col-xs-12 col-md-6">
-           <div className="aboutcontent" >
-               <h4 className="row">Who are we?</h4>
-               <p className="row"> We are a group of Students who are at Fullstack Academy blah balh blahdsflkajdlkfjdijsadlnn kldjslafjl dsfsldkfj s</p>
-           </div>
-         </div>
-      </div>
-      <div className="container" id="contact">
-        <h2> Meet Us </h2>
-        <div className="row" id="us">
-          <div className="col-xs-12 col-sm-6 col-md-3" id="jake">
-            <h4>Jake</h4>
-            <img src={defaultPix}/>
-            <p>Hi, I am Jake Peyser</p>
+      <div className="container" id="about">
+        <div className="aboutus" id="whoami">
+          <h2> About Us</h2>
+        </div>
+        <div className="row" id="aboutinfo">
+          <div className="col-xs-12 col-md-6">
+            <img src={require('../../../src/images/work.jpg')} id="work"></img>
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-3" id="emily">
-            <h4>Emily</h4>
-            <img src={defaultPix}/>
-            <p>Hi, I am Mike</p>
+          <div className="col-xs-12 col-md-6">
+            <div className="aboutcontent" >
+                <h4 className="row">Who are we?</h4>
+                <p className="row"> We are a group of Students who are at Fullstack Academy blah balh blahdsflkajdlkfjdijsadlnn kldjslafjl dsfsldkfj s</p>
+            </div>
           </div>
-          <div className="col-xs-12 col-sm-6 col-md-3" id="mike">
-            <h4>Mike</h4>
-            <img src={defaultPix}/>
-            <p>Hi, Emily</p>
-          </div>
-          <div className="col-xs-12 col-sm-6 col-md-3" id="hiten">
-            <h4>Hiten</h4>
-            <img src={defaultPix}/>
-            <p>Hi, Hiten</p>
+        </div>
+        <div className="container" id="contact">
+          <h2> Meet Us </h2>
+          <div className="row" id="us">
+            <div className="col-xs-12 col-sm-6 col-md-3" id="jake">
+              <h4>Jake</h4>
+              <img src={defaultPix}/>
+              <p>Hi, I am Jake Peyser</p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3" id="emily">
+              <h4>Emily</h4>
+              <img src={defaultPix}/>
+              <p>Hi, I am Mike</p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3" id="mike">
+              <h4>Mike</h4>
+              <img src={defaultPix}/>
+              <p>Hi, Emily</p>
+            </div>
+            <div className="col-xs-12 col-sm-6 col-md-3" id="hiten">
+              <h4>Hiten</h4>
+              <img src={defaultPix}/>
+              <p>Hi, Hiten</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 
   </MuiThemeProvider>
 )
