04dc794e24f4d366f3728d4efff0174fe3762aa1
Jake Peyser <jakepeyser@gmail.com>
2016-12-03 14:11:04 -0500

    general home page complete

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -1,28 +1,62 @@
 // import redux, react
 import React from 'react'
-import { connect } from 'react-redux';
 
 // Material theme
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {Paper} from 'material-ui';
+import { FontIcon } from 'material-ui';
 
-//External Components Modular
+// Custom components
 import Signin from './Signin'
 
-//About us
-const defaultPix = '../../../src/images/defaultProfile.png';
-
+// Page content
 const navbarLinks = {
   about: 'About Us',
   contact: 'Contact'
 }
-
+const teamMembers = [
+  {
+    name: 'Emily Ho',
+    img: '../../../src/images/coders/emily_ho.jpeg',
+    links: {
+      github: 'https://github.com/emilydev',
+      linkedin: 'https://www.linkedin.com/in/emily-ho-8a035235',
+      email: 'mailto:emilyyho33@gmail.com'
+    }
+  },
+  {
+    name: 'Hiten Patel',
+    img: '../../../src/images/coders/hiten_patel.png',
+    links: {
+      github: 'https://github.com/Hitenp1',
+      email: 'mailto:hpduke2@gmail.com'
+    }
+  },
+  {
+    name: 'Jake Peyser',
+    img: '../../../src/images/coders/jake_peyser.jpeg',
+    links: {
+      github: 'https://github.com/jakepeyser',
+      linkedin: 'https://www.linkedin.com/in/jakepeyser',
+      email: 'mailto:jakepeyser@gmail.com'
+    }
+  },
+  {
+    name: 'Mike Williams',
+    img: '../../../src/images/coders/mike_williams.jpg',
+    links: {
+      github: 'https://github.com/mkewls',
+      linkedin: 'https://www.linkedin.com/in/mewill',
+      email: 'mailto:mw@mikewill.net'
+    }
+  }
+]
 
 // -=-=-=-=-=-= COMPONENT =-=-=-=-=-=-
 
 export default () => (
   <MuiThemeProvider>
     <div id="main">
+      {/* Main header banner */}
       <div className="container" id="home">
         <nav className="home-nav">
           <div className="navbar-header">
@@ -62,48 +96,60 @@ export default () => (
         </div>
       </div>
 
-      <div className="container" id="about">
-        <div className="aboutus" id="whoami">
-          <h2> About Us</h2>
-        </div>
-        <div className="row" id="aboutinfo">
-          <div className="col-xs-12 col-md-6">
+      {/* Methodology section */}
+      <div className="section">
+        <div className="section-content">
+          <div className="img-wrapper">
             <img src={require('../../../src/images/work.jpg')} id="work"></img>
           </div>
-          <div className="col-xs-12 col-md-6">
-            <div className="aboutcontent" >
-                <h4 className="row">Who are we?</h4>
-                <p className="row"> We are a group of Students who are at Fullstack Academy blah balh blahdsflkajdlkfjdijsadlnn kldjslafjl dsfsldkfj s</p>
-            </div>
-          </div>
-        </div>
-        <div className="container" id="contact">
-          <h2> Meet Us </h2>
-          <div className="row" id="us">
-            <div className="col-xs-12 col-sm-6 col-md-3" id="jake">
-              <h4>Jake</h4>
-              <img src={defaultPix}/>
-              <p>Hi, I am Jake Peyser</p>
-            </div>
-            <div className="col-xs-12 col-sm-6 col-md-3" id="emily">
-              <h4>Emily</h4>
-              <img src={defaultPix}/>
-              <p>Hi, I am Mike</p>
-            </div>
-            <div className="col-xs-12 col-sm-6 col-md-3" id="mike">
-              <h4>Mike</h4>
-              <img src={defaultPix}/>
-              <p>Hi, Emily</p>
-            </div>
-            <div className="col-xs-12 col-sm-6 col-md-3" id="hiten">
-              <h4>Hiten</h4>
-              <img src={defaultPix}/>
-              <p>Hi, Hiten</p>
-            </div>
+          <div className="content-text">
+            <h3>Our Methodology</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet erat at neque accumsan malesuada in et purus. Suspendisse potenti. Aenean et tellus a turpis gravida sodales id a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent tortor velit, accumsan sit amet maximus iaculis, condimentum quis magna. Fusce ac nulla tempor, vehicula risus vel, feugiat elit. Nulla ac quam ligula. Praesent pellentesque et sapien non fringilla.</p>
           </div>
         </div>
       </div>
-    </div>
 
+      {/* Process section */}
+      <div className="section">
+        <div className="section-content">
+          <div className="img-wrapper">
+            <img src={require('../../../src/images/work.jpg')} id="work"></img>
+          </div>
+          <div className="content-text">
+            <h3>The Process</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet erat at neque accumsan malesuada in et purus. Suspendisse potenti. Aenean et tellus a turpis gravida sodales id a enim. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent tortor velit, accumsan sit amet maximus iaculis, condimentum quis magna. Fusce ac nulla tempor, vehicula risus vel, feugiat elit. Nulla ac quam ligula. Praesent pellentesque et sapien non fringilla.</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Contact section */}
+      <div className="section team">
+        <h3> The Team </h3>
+        <div className="coders">
+          {
+            // Display each team member bubble
+            teamMembers.map((coder, idx) => (
+              <div key={ idx } className="coder-wrapper">
+                <div className="coder">
+                  <img src={coder.img} alt={coder.name} />
+                  <div className="coder-data">
+                    <h4>{coder.name}</h4>
+                    <div className="coder-links">
+                    {
+                      Object.keys(coder.links).map((key, idx) =>
+                        <a key={ idx } href={ coder.links[key] } target="_blank">
+                          <FontIcon className="material-icons" color="white">home</FontIcon>
+                        </a>
+                      )
+                    }
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
   </MuiThemeProvider>
 )
