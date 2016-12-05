9204fe84111f77326df4b0fdeec93c2e6aa6697f
Jake Peyser <jakepeyser@gmail.com>
2016-12-05 11:34:01 -0500

    squashing bugs and taking names

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -98,7 +98,7 @@ export default () => (
             <a href="#">
               <svg width="45px" height="30px" viewBox="0 0 204 137">
                 <g id="Page-1" stroke="none" strokeWidth="1" fill="none">
-                  <g id="Group" transform="translate(0.000000, -0.362180)" fill="#FFFFFF">
+                  <g id="logo-svg" transform="translate(0.000000, -0.362180)">
                     <path d="M0,108.36218 L204,108.36218 L204,137.36218 L0,137.36218 L0,108.36218 Z M2,110.36218 L202,110.36218 L202,135.36218 L2,135.36218 L2,110.36218 Z" id="Shape"></path>
                     <path d="M50,54.36218 L204,54.36218 L204,83.36218 L50,83.36218 L50,54.36218 Z M52,56.36218 L202,56.36218 L202,81.36218 L52,81.36218 L52,56.36218 Z" id="Shape"></path>
                     <path d="M100,0.36218 L204,0.36218 L204,29.36218 L100,29.36218 L100,0.36218 Z M102,2.36218 L202,2.36218 L202,27.36218 L102,27.36218 L102,2.36218 Z" id="Shape"></path>
@@ -144,7 +144,7 @@ export default () => (
         <div className="section-content">
           <div className="img-wrapper">
             <img
-              src={require('../../../src/images/sunsetjump.png')}
+              src={require('../../../src/images/home/sunsetjump.png')}
               className="img-circle img-responsive"
               style={ aboutImagesStyle }/>
           </div>
@@ -166,7 +166,7 @@ export default () => (
         <div className="section-content">
           <div className="img-wrapper">
             <img
-              src={require('../../../src/images/work.png')}
+              src={require('../../../src/images/home/work.png')}
               className="img-circle img-responsive"
               style={ aboutImagesStyle }/>
           </div>
