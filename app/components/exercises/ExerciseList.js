1a367ee75e50ce0e04b8597fee8d09fedc95eacc
mkewls <mw@mikewill.net>
2016-12-07 12:23:08 -0500

    changed tables to flex-box, removed delete on mobile

--- app/components/exercises/ExerciseList.js
+++ app/components/exercises/ExerciseList.js
@@ -7,8 +7,7 @@ import { deleteExercise } from '../../reducers/exercises'
 import InfoItem from '../widgets/InfoItem'
 import AddExerciseContainer from './AddExerciseContainer'
 //Material UI
-import { Table, TableHeader, TableHeaderColumn,
-         TableBody, TableRow, TableRowColumn,Divider, TableFooter, IconButton, FontIcon} from 'material-ui'
+import { Divider, IconButton, FontIcon} from 'material-ui'
 
 import { StepsRaisedButton, StepsActionButton } from '../material-style'
 import { textDark, errorText } from '../colors';
@@ -42,58 +41,56 @@ export class ExerciseList extends Component {
 
 render() {
     const { user, exercises, deleteExercise } = this.props;
-    
+
     return (
 
-  <div id="exercise-list" >
-    <Helmet title="Exercises" />
-    <h1 className="page-header">Exercises</h1>
-    
-    <AddExerciseContainer user={user} /> 
-    <hr/>
+      <div id="exercise-list" >
+        <Helmet title="Exercises" />
+        <h1 className="page-header">Exercises</h1>
 
-    <div className="mobile_table">
-    <Table style={{backgroundColor:'none'}} >
-      <TableBody displayRowCheckbox={false}>
-      {
-        exercises && exercises.sort((a, b) => {
-          if (b.title.toLowerCase() < a.title.toLowerCase()) return 1;
-          else if (b.title.toLowerCase() > a.title.toLowerCase()) return -1;
-          else return 0;
-        }).map( exercise =>
-          <TableRow key={ exercise.id } selectable={false} className="exercise-row">
-            <TableRowColumn className="img-col">
-              <img src={exercise.img_url}></img>
-            </TableRowColumn>
-            <TableRowColumn style={{wordWrap: 'break-word', whiteSpace: 'normal'}} >
-              <div className="exercise-data">
-                <h3>{exercise.title}</h3>
-                <InfoItem icon="description" label="Description"  content={exercise.description}/>
-              </div>
-            </TableRowColumn>
-            <TableRowColumn style={{width:"10px", "paddingRight": "8em"}}>
-                <StepsActionButton mini={ true }
-                  backgroundColor={ errorText }
-                  onTouchTap={ () => this.dialogOpen(user.id, exercise.id) } >
-                  <FontIcon className={'material-icons'}>clear</FontIcon>
-                </StepsActionButton>
-            </TableRowColumn>
-          </TableRow>
-        )
-      }
-      </TableBody>
-    </Table>
+        <AddExerciseContainer user={user} />
+        <hr/>
+
+        <div className="exercise-rows">
+          {
+            exercises && exercises.sort((a, b) => {
+              if (b.title.toLowerCase() < a.title.toLowerCase()) return 1;
+              else if (b.title.toLowerCase() > a.title.toLowerCase()) return -1;
+              else return 0;
+            }).map( exercise => {
+              return (
+                <div>
+                  <div key={ exercise.id } className="exercise">
+                    <div id="exercise-img">
+                      <img src={exercise.img_url} className="img-responsive"></img>
+                    </div>
+                    <div id="exercise-info">
+                      <h3 id="exercise-title">{ exercise.title }</h3>
+                      <p id="exercise-description"> {exercise.description }</p>
+                    </div>
+                    <div id="exercise-remove">
+                      <StepsActionButton mini={ true }
+                        backgroundColor={ errorText }
+                        onTouchTap={ () => this.dialogOpen(user.id, exercise.id) } >
+                        <FontIcon className={'material-icons'}>clear</FontIcon>
+                      </StepsActionButton>
+                    </div>
+                  </div>
+                  <Divider />
+                </div>
+                )})
+          }
+          <ConfirmDialog
+            title="Confirm Exercise Deletion"
+            isOpen={ this.state.confirmOpen }
+            confirm={ () => deleteExercise(this.state.userId, this.state.exerciseId) }
+            dialogClose={ this.dialogClose }>
+            {`Would you like to permenantly delete this exercise?`}
+          </ConfirmDialog>
+      </div>
     </div>
-    <ConfirmDialog
-          title="Confirm Exercise Deletion"
-          isOpen={ this.state.confirmOpen }
-          confirm={ () => deleteExercise(this.state.userId, this.state.exerciseId) } 
-          dialogClose={ this.dialogClose }>
-          {`Would you like to permenantly delete this exercise?`}
-      </ConfirmDialog>
-  </div>
-  )
- }
+    )
+  }
 }
 
 // -=-=-=-=-= CONTAINER =-=-=-=-=-=-
